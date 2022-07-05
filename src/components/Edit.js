b5d1605b80aa4020f6ed90c95d81d40e8717513d
Fernanda <fernanda@MacBook-Air-de-Fernanda.local>
2022-07-05 11:30:59 -0400

    edit templates card of notes

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -4,16 +4,16 @@ import { getDoc, updateDoc, doc } from "firebase/firestore"
 import { db } from "../firebaseConfig/firebase"
 
 const Edit = () => {
-    const [ title, setTitle] = useState('')
-    const [ content, setContent ] = useState('')
-    const [ lastEdition, setLastEdition ] = useState('')
-    const [ day, setDay ] = useState('')
-    const [ hour, setHour ] = useState('')
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+    const [lastEdition, setLastEdition] = useState('')
+    const [day, setDay] = useState('')
+    const [hour, setHour] = useState('')
 
     const navigate = useNavigate()
-    const {id} = useParams()
+    const { id } = useParams()
 
-    const update = async (e) =>{
+    const update = async (e) => {
         e.preventDefault()
         const note = doc(db, 'notes', id)
         const data = {
@@ -26,18 +26,31 @@ const Edit = () => {
         await updateDoc(note, data)
         navigate('/')
     }
-    const getNoteById = async(id) => {
+    const getNoteById = async (id) => {
         const note = await getDoc(doc(db, 'notes', id))
-        console.log(note.data())
+        if (note.data() !== undefined) {
+            console.log(note)
+            setTitle(note.data().title)
+            setContent(note.data().content)
+            setLastEdition(note.data().lastEdition)
+            setDay(note.data().day)
+            setHour(note.data().hour)
+        } else {
+            console.log('La nota no existe')
+        }
     }
-    useEffect( () => {
+    useEffect(() => {
         getNoteById(id)
     }, [])
 
-
-
     return (
-        <div>Edit</div>
+        <>
+            <div>Edit</div>
+            <form onSubmit = { update }>
+                <label>Título</label>
+
+            </form>
+        </>
     )
 }
 
